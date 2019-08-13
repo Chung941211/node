8e2498fb8ff290cbafccf91a22318f7170f10901
Chung941211 <zhaoxiaocong@jperation.com>
2019-08-13 20:03:43 +0800

    feat(node): 增加连接数据库文件

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * config 配置文件
+ */
+
+ exports.MONGODB =  {
+    url: `mongodb://localhost:27017/forum`
+ }
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,15 @@
+const models = require('../models/list');
+const List = models.list;
+
 exports.home = ((req, res, next) => {
+  let tempList = new List({
+    title: '测试1',
+    content: '测试内容'
+  })
+
+  tempList.save().then(data => responseClient(res, 200, 0, '保存成功', data))
+  .catch(err => responseClient(err))
+  
   res.render('index', {
       title: '首页'
   })
