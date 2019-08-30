65346d1e8cb7cbe952fc75c65d219d4368cd82cb
zhaoxiaocong <zhaoxiaocong@jperation.com>
2019-08-30 20:38:37 +0800

    feat(node): 增加自增组件

--- common/server.js
+++ common/server.js
@@ -2,21 +2,30 @@
  * MongoDB 连接模块
  */
 
- const config   = require('../config')
- const mongoose = require('mongoose')
- const consola  = require('consola')
- 
- exports.connect = () => {
-   
+const config   = require('../config'),
+mongoose = require('mongoose'),
+consola  = require('consola'),
+autoIncrement = require('mongoose-auto-increment')
+
+
+exports.connect = () => {
+    
   mongoose.connect(config.MONGODB.url, {
+    useCreateIndex: true,
     useNewUrlParser: true
   })
 
   mongoose.connection.on('error', error => consola.warn('MongoDB 连接失败!', error))
 
   mongoose.connection.on('open', () => consola.ready('MongoDB 连接成功'))
-  
-  return mongoose
- }
 
- exports.mongoose = mongoose
+  return mongoose
+}
+
+autoIncrement.initialize(mongoose.connection)
+
+exports.Schema = mongoose.Schema
+
+exports.autoIncrement = autoIncrement
+
+exports.mongoose = mongoose
