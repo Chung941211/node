175bb857d61150b45a60345472a284cb9deb68d6
Chung941211 <zhaoxiaocong@jperation.com>
2019-09-03 18:40:31 +0800

    feat(node): 文章增加时间

--- models/posting.js
+++ models/posting.js
@@ -4,9 +4,15 @@ const posingSchema = new Schema({
     
     title: { type: String, required: true, defalut: ''}, // 文章标题
 
+    description: { type: String, default: '' }, // 简介
+
     author: { type: String, default: '' }, // 作者
     
-    content: { type: String, defalut: ''} // 内容
+    content: { type: String, defalut: ''}, // 内容
+
+    create_time: { type: Date, default: Date.now }, // 创建日期
+
+    update_time: { type: Date, defalut: Date.now }
 })
 
 posingSchema.plugin(autoIncrement.plugin, {
